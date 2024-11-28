696be400004c1aaf7d27a5141ee3f3c06dbdb769
Valentin <luginbuhlvalentin@gmail.com>
2024-11-28 17:50:54 +0100

    disabled player/spectator buttons

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -4,9 +4,9 @@
         <p class="rules">How to play? <mark class="default">Each player, black and white, takes turns placing a piece of their color on the board following specific rules. The game ends when neither player can place a piece, and the player with the most pieces of their color on the board wins.</mark></p>
 
         <div class="view">
-            <router-link to="/player"><button class="default">Player<img src="../assets/icons/player.png" alt="" width="30"></button></router-link>
+            <router-link to="/player"><button class="default" disabled="true">Player<img src="../assets/icons/player.png" alt="" width="30"></button></router-link>
             <router-link to="/multiplayer"><button class="alternative">Multiplayer<img src="../assets/icons/multiplayer.png" alt="" width="30"></button></router-link>
-            <router-link to="/spectator"><button class="alternative">Spectator<img src="../assets/icons/spectator.png" alt="" width="30"></button></router-link>
+            <router-link to="/spectator"><button class="alternative" disabled="true">Spectator<img src="../assets/icons/spectator.png" alt="" width="30"></button></router-link>
         </div>
     </div>
 </template>
